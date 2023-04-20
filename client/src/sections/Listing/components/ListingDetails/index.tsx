7ddb0a882d29270ac4b5d87b97c72d8965f4555e
Fitsum <fitsesile@gmail.com>
2023-04-20 14:27:13 +0300

    Lisiting bookings, details, reviews and update added,

---
+++ client/src/sections/Listing/components/ListingDetails/index.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Avatar, Divider, Rate, Tag, Typography } from "antd";
+import { EnvironmentOutlined } from "@ant-design/icons";
+import { Listing as ListingData } from "../../../../lib/graphql/queries/Listing/__generated__/Listing";
+import { iconColor } from "../../../../lib/utils";
+// import { ShowLocation } from "../../../../lib/components/Map";
+import { Viewer } from "../../../../lib/types";
+
+interface Props {
+    listing: ListingData["listing"];
+    viewer: Viewer;
+}
+
+const { Paragraph, Title } = Typography;
+
+export const ListingDetails = ({ listing, viewer }: Props) => {
+    const {
+        title,
+        description,
+        image,
+        type,
+        address,
+        city,
+        host,
+        rating,
+        numReviews,
+    } = listing;
+
+    return (
+        <div className="listing-details">
+            <div
+                style={{ backgroundImage: `url(${image})` }}
+                className="listing-details__image"
+            />
+
+            <div className="listing-details__information">
+                <Paragraph
+                    type="secondary"
+                    ellipsis
+                    className="listing-details__city-address"
+                >
+                    <Link to={`/listings/${city}`}>
+                        <EnvironmentOutlined style={{ color: iconColor }} />{" "}
+                        {city}
+                    </Link>
+                    <Divider type="vertical" />
+                    {address}
+                </Paragraph>
+                <Title level={3} className="listing-details__title">
+                    {title}
+                </Title>
+                <Rate disabled defaultValue={rating} />{" "}
+                <Title level={5} style={{ display: "inline" }}>
+                    ({numReviews} reviews)
+                </Title>
+            </div>
+
+            <Divider />
+
+            <div className="listing-details__section">
+                <Link to={`/user/${host.id}`}>
+                    <Avatar src={host.avatar} size={64} />
+                    <Title level={2} className="listing-details__host-name">
+                        {host.name}
+                    </Title>
+                </Link>
+            </div>
+
+            <div style={{ display: viewer.token && viewer.id === host.id ? "none" : "block" }}>
+                <Link
+                    to={
+                        viewer.id && viewer.token
+                            ? `/chat/${host.id}`
+                            : "/login"
+                    }
+                >
+                    <Title level={5}>
+                        <Tag color={iconColor}>Message</Tag> {host.name} to know
+                        more.
+                    </Title>
+                </Link>
+            </div>
+
+            <Divider />
+
+            <div className="listing-details__section">
+                <Title level={4}>About this item</Title>
+                <div className="listing-details__about-items">
+                    <Tag color="magenta">{type}</Tag>
+                </div>
+                <Paragraph ellipsis={{ rows: 3, expandable: true }}>
+                    {description}
+                </Paragraph>
+            </div>
+
+            {/* <ShowLocation address={address} /> */}
+        </div>
+    );
+};
---
+++ client/src/sections/Listing/components/ListingReviews/index.tsx
@@ -0,0 +1,231 @@
+import { WechatOutlined } from "@ant-design/icons";
+import { useMutation } from "@apollo/client";
+import { Avatar, Button, Comment, Form, Input, List, Rate, Tooltip, Typography } from "antd";
+import { DeleteFilled } from "@ant-design/icons";
+import moment from "moment";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { CREATE_REVIEW } from "../../../../lib/graphql/mutations/CreateReview";
+import {
+    CreateReview as CreateReviewData,
+    CreateReviewVariables,
+} from "../../../../lib/graphql/mutations/CreateReview/__generated__/CreateReview";
+import { Listing } from "../../../../lib/graphql/queries/Listing/__generated__/Listing";
+import { Viewer } from "../../../../lib/types";
+import { displayErrorMessage, displaySuccessNotification, iconColor } from "../../../../lib/utils";
+import { DeleteReview as DeleteReviewData, DeleteReviewVariables } from "../../../../lib/graphql/mutations/DeleteReview/__generated__/DeleteReview";
+import { DELETE_REVIEW } from "../../../../lib/graphql/mutations/DeleteReview";
+const { TextArea } = Input;
+
+const { Title, Text } = Typography;
+
+interface ReviewItem {
+    author: string;
+    avatar: React.ReactNode;
+    content: React.ReactNode;
+    datetime: React.ReactNode;
+}
+
+interface Props {
+    reviews: Listing["listing"]["reviews"];
+    viewer: Viewer;
+    refetchListing: () => Promise<void>;
+    listingId: string;
+}
+
+interface EditorProps {
+    onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+    onSubmit: () => void;
+    submitting: boolean;
+    value: string;
+    ratingValue: number;
+    onRatingChange: ((value: number) => void) | undefined;
+}
+
+const CommentList = ({ reviews }: { reviews: ReviewItem[] }) => (
+    <List
+        dataSource={reviews}
+        header={
+            <Title level={4}>
+                <WechatOutlined />
+                <br />
+                ({reviews.length} {reviews.length > 1 ? "reviews" : "review"})
+            </Title>
+        }
+        itemLayout="horizontal"
+        renderItem={(props) => <Comment {...props} />}
+    />
+);
+
+const Editor = ({ onChange, onSubmit, submitting, value, ratingValue, onRatingChange }: EditorProps) => (
+    <>
+        <Form.Item>
+            <TextArea rows={4} onChange={onChange} value={value} />
+        </Form.Item>
+
+        <Form.Item>
+            <Rate allowHalf value={ratingValue} onChange={onRatingChange}/>
+        </Form.Item>
+
+        <Form.Item>
+            <Button
+                htmlType="submit"
+                loading={submitting}
+                onClick={onSubmit}
+                type="primary"
+            >
+                Add Review
+            </Button>
+        </Form.Item>
+    </>
+);
+
+export const CreateReview: React.FC<Props> = ({reviews, viewer, refetchListing, listingId}) => {
+    const [value, setValue] = useState("");
+    const [ratingValue, setRatingValue] = useState(0);
+
+    const [createReview, { loading }] = useMutation<
+        CreateReviewData,
+        CreateReviewVariables
+    >(CREATE_REVIEW, {
+        onCompleted: () => {
+            displaySuccessNotification(
+                "Thanks for reviewing the listing!"
+            );
+            refetchListing();
+            setRatingValue(0);
+            setValue("");
+        },
+        onError: (err) => {
+            console.log(err.message)
+            displayErrorMessage(
+                err.message
+            );
+        },
+    });
+
+    const [deleteReview, { loading: loadingDeleteReview }] = useMutation<
+        DeleteReviewData,
+        DeleteReviewVariables
+    >(DELETE_REVIEW, {
+        onCompleted: () => {
+            displaySuccessNotification("Your review has been deleted!");
+            refetchListing();
+        },
+        onError: (err) => {
+            console.log(err.message);
+            displayErrorMessage(err.message);
+        },
+    });
+
+    const handleDeleteReview = (reviewId: string) => {
+        deleteReview({
+            variables: {
+                input: {
+                    reviewId,
+                    listingId
+                }
+            }
+        })
+    }
+
+    const modifiedReviews: ReviewItem[] = reviews.map((review) => {
+        return {
+            avatar: (
+                <Link to={`/user/${review.author.id}`}>
+                    <Avatar
+                        src={review.author.avatar}
+                        alt={review.author.name}
+                    />
+                </Link>
+            ),
+            author: review.author.name,
+            content: (
+                <div
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "25px",
+                    }}
+                >
+                    <div>
+                        <Rate disabled defaultValue={review.rating} />
+                        <p>{review.comment}</p>
+                    </div>
+
+                    {viewer.id && viewer.id === review.author.id && (
+                        <DeleteFilled
+                            style={{
+                                color: "#ce1313",
+                                fontSize: "16px",
+                                cursor: "pointer",
+                            }}
+                            onClick={() => handleDeleteReview(review.id)}
+                        />
+                    )}
+                </div>
+            ),
+            datetime: (
+                <Tooltip
+                    title={moment(review.createdAt).format("YYYY-MM-DD HH:mm")}
+                >
+                    <span>{moment(review.createdAt).fromNow()}</span>
+                </Tooltip>
+            ),
+        };
+    })
+
+    const handleSubmit = () => {
+        createReview({
+            variables: {
+                input: {
+                    rating: ratingValue,
+                    comment: value,
+                    listingId: listingId
+                }
+            }
+        })
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setValue(e.target.value);
+    };
+
+    const handleRatingChange = (value: number) => {
+        setRatingValue(value)
+    };
+
+    const alreadyReviewed = reviews.some((review) => review.author.id === viewer.id);
+
+    return (
+        <>
+            {modifiedReviews.length > 0 && (
+                <CommentList reviews={modifiedReviews} />
+            )}
+
+            {!viewer.id ? (
+                <Text type="secondary" mark>
+                    Sign in to rate this listing!
+                </Text>
+            ) : viewer.id && !alreadyReviewed ? (
+                <Comment
+                    avatar={<Avatar src={viewer.avatar} alt={viewer.id} />}
+                    content={
+                        <Editor
+                            onChange={handleChange}
+                            onSubmit={handleSubmit}
+                            submitting={loading}
+                            value={value}
+                            onRatingChange={handleRatingChange}
+                            ratingValue={ratingValue}
+                        />
+                    }
+                />
+            ) : (
+                <Text type="secondary" mark>
+                    You have already rated this listing!
+                </Text>
+            )}
+        </>
+    );
+};
